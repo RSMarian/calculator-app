f7fe85fa23a7792d0b63d48fa5954731a5a85e2c
RSMarian <marianstefan1499@gmail.com>
2022-12-21 21:17:59 +0200

    declared DOM variables

---
+++ app.js
@@ -0,0 +1,6 @@
+function main () {
+    let result = document.querySelector(".result");
+    let buttons = document.querySelectorAll(".buttons");
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal");
+}
